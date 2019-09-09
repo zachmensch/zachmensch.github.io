c0e53d183541a26637a2c7b23c5eb0bea4da06d6
zachmensch <zacharym1@sasktel.net>
2019-09-09 13:57:39 -0600

    added lineart project

---
+++ lineart/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+}
+
+function draw() {
+  fill(155);
+  ellipse(mouseX, mouseY, 100, 100);
+}
