3bba55a8bd27e6e967881c56d92066da741ad385
zachmensch <zacharym1@sasktel.net>
2019-09-13 14:14:09 -0600

    did in class work + small work on title screen for tower defense game

--- First Project/sketch.js
+++ First Project/sketch.js
@@ -5,10 +5,21 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+    background(255,255,102);
+    fill(0,150,255);
+    rect(650, 345, 300, 100);
+    rect(650, 475, 300, 100);
+    fill(0,0,0)
+    textAlign(CENTER);
+    textSize(20);
+    text("Options", 800, 530);
+    text("Start", 800, 400);
+    textSize(100);
+    text("Tower Defense Game", 800, 100);
 }
 
 function draw() {
 
+console.log(mouseX, mouseY);
 }
 
--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -1,17 +1,33 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// stuff
+// zach
+// 2019
 
+let hat;
+
+let scalar = 1;
+
+function preload() {
+  hat = loadImage("assets/hat.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220);
 }
 
 function draw() {
-  fill(155);
-  ellipse(mouseX, mouseY, 100, 100);
+  background(255);
+  //noCursor();
+
+  if (keyIsPressed) {
+    if (keyCode === UP_ARROW) {
+      scalar *= 1.02
+    }
+    else if (keyCode === DOWN_ARROW) {
+      scalar /= 1.02   
+    }
+
+  }
+  image(hat, mouseX, mouseY, hat.width * scalar, hat.height * scalar);
+  imageMode(CENTER);
 }
+
