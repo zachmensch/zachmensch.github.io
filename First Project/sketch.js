ef2fe3795ad59fac87f6f57f2a4d66ca3229c0f7
zachmensch <zacharym1@sasktel.net>
2019-09-16 15:18:45 -0600

    working on button

--- First Project/sketch.js
+++ First Project/sketch.js
@@ -3,13 +3,23 @@
 // September 11th, 2019
 //
 
-let mode;
+let screen;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
     background(255,255,102);
     mainMenu();
-    screen = "titlescreen";
+    // fill(0,150,255);
+    // rect(650, 345, 300, 100);
+    // rect(650, 475, 300, 100);
+    // fill(0,0,0)
+    // textAlign(CENTER);
+    // textSize(20);
+    // text("Options", 800, 530);
+    // text("Start", 800, 400);
+    // textSize(100);
+    // text("Tower Defense Game", 800, 100);
+    // screen = "titlescreen";
 }
 
 function draw() {
@@ -32,9 +42,12 @@ function mainMenu() {
 
 function clickButton() {
   if (mouseClicked) {
-    if (mode = "titlescreen") {
-      if (mouseX )
-    }
+    if (screen = "titlescreen") {
+      if (mouseX > 650 && mouseX < 950 && mouseY > 345 && mouseY < 440) {
+        return true;
+      }
 
+
+    } 
   }
 }
--- lineart/sketch.js
+++ lineart/sketch.js
@@ -3,9 +3,14 @@
 // September 9th, 2019
 //
 
+let fr = 120;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
+  frameRate(fr);
+
+  
 }
 
 function draw() {
