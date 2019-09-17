ccd48a9bc0085b43d5db7e1de4545185ca5f84ca
zachmensch <zacharym1@sasktel.net>
2019-09-17 13:32:11 -0600

    working on button

--- First Project/sketch.js
+++ First Project/sketch.js
@@ -7,47 +7,41 @@ let screen;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(255,255,102);
-    mainMenu();
-    // fill(0,150,255);
-    // rect(650, 345, 300, 100);
-    // rect(650, 475, 300, 100);
-    // fill(0,0,0)
-    // textAlign(CENTER);
-    // textSize(20);
-    // text("Options", 800, 530);
-    // text("Start", 800, 400);
-    // textSize(100);
-    // text("Tower Defense Game", 800, 100);
-    // screen = "titlescreen";
+    screen = "titlescreen";
 }
 
 function draw() {
-
-console.log(mouseX, mouseY);
+  screenDisplay();
+  mouseReleased();
+  console.log(mouseX, mouseY);
+  console.log(screen)
 }
 
-function mainMenu() {
-  fill(0,150,255);
-  rect(650, 345, 300, 100);
-  rect(650, 475, 300, 100);
-  fill(0,0,0)
-  textAlign(CENTER);
-  textSize(20);
-  text("Options", 800, 530);
-  text("Start", 800, 400);
-  textSize(100);
-  text("Tower Defense Game", 800, 100);
-}
-
-function clickButton() {
-  if (mouseClicked) {
-    if (screen = "titlescreen") {
-      if (mouseX > 650 && mouseX < 950 && mouseY > 345 && mouseY < 440) {
-        return true;
-      }
-
-
-    } 
+function screenDisplay() {
+  if (screen = "titlescreen"){
+    background(255,255,102);
+    fill(0,150,255);
+    rect(650, 345, 300, 100);
+    rect(650, 475, 300, 100);
+    fill(0,0,0)
+    textAlign(CENTER);
+    textSize(20);
+    text("Options", 800, 530);
+    text("Start", 800, 400);
+    textSize(100);
+    text("Tower Defense Game", 800, 100);
   }
 }
+
+function mouseReleased() {
+  if (screen = "titlescreen") {
+    if (mouseX > 650 && mouseX < 950 && mouseY > 345 && mouseY < 440) {
+      screen = "gamescreen";
+    }
+    if (mouseX > 650 && mouseX < 950 && mouseY > 475 && mouseY < 575) {
+      screen = "options";
+    }
+  }
+}
+
+
