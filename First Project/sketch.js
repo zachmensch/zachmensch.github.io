2df7fcb126651767c0041f9e49392c5bf2404d43
zachmensch <zacharym1@sasktel.net>
2019-09-17 14:11:33 -0600

    made buttons work, started adding keyboard controls for player character

--- First Project/sketch.js
+++ First Project/sketch.js
@@ -7,18 +7,20 @@ let screen;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    screen = "titlescreen";
+  screen = "titleScreen";
 }
 
 function draw() {
   screenDisplay();
-  mouseReleased();
+  mousePressed();
+  characterControl();
   console.log(mouseX, mouseY);
   console.log(screen)
 }
 
 function screenDisplay() {
-  if (screen = "titlescreen"){
+  if (screen === "titleScreen"){
+    // Display for Title Screen
     background(255,255,102);
     fill(0,150,255);
     rect(650, 345, 300, 100);
@@ -30,17 +32,50 @@ function screenDisplay() {
     text("Start", 800, 400);
     textSize(100);
     text("Tower Defense Game", 800, 100);
+    textSize(15);
+    text("Made by Zach Mensch & Matthew Resendes", 800, 750);
+  }
+  if (screen === "gamesScreen") {
+    background(255,255,102);
+  }
+  if (screen === "optionScreen"){
+    // Display for Options Screen
+    background(255,255,102);
+    fill(0,150,255);
+    rect(650, 605, 300, 100); 
+    fill(0,0,0);
+    textAlign(CENTER);
+    textSize(20);
+    text("Back", 800, 660)
   }
 }
 
-function mouseReleased() {
-  if (screen = "titlescreen") {
-    if (mouseX > 650 && mouseX < 950 && mouseY > 345 && mouseY < 440) {
-      screen = "gamescreen";
+function mousePressed() {
+  if (mouseIsPressed) {
+    // Title Screen Buttons
+    if (screen === "titleScreen") {
+      if (mouseX > 650 && mouseX < 950 && mouseY > 345 && mouseY < 440) {
+        screen = "gamesScreen";
+        clear();
+      }
+      if (mouseX > 650 && mouseX < 950 && mouseY > 475 && mouseY < 575) {
+        screen = "optionScreen";
+        clear();
+      }
     }
-    if (mouseX > 650 && mouseX < 950 && mouseY > 475 && mouseY < 575) {
-      screen = "options";
+    // Options Screen Buttons
+    if (screen == "optionScreen") {
+      if (mouseX > 650 && mouseX < 950 && mouseY > 605 && mouseY < 705) {
+        screen = "titleScreen";
+      }
     }
+  }
+}
+
+function characterControl() {
+  if (screen === "gameScreen") {
+    fill(0);
+    ellipse(500, 500, 100);
   }
 }
 
